be9eb06219bd18b0b29e4069e5b4a250303f3566
RamonRomeroTec <rr100@live.com.mx>
2019-11-02 01:34:58 -0600

    quit subprocessing and queues

--- src/master.py
+++ src/master.py
@@ -6,56 +6,37 @@ with open('./settings.json') as f:
     settings = json.load(f)
 
 NUM_SLAVES = len(settings['slaves'])
-SLAVE_SOCKETS = [(s['ip'], s['port']) for s in settings['slaves']]
 MASTER_HOST = settings['master']['ip']  # The server's hostname or IP address
 MASTER_PORT = settings['master']['port']        # The port used by the server
 
-from multiprocessing import Process
-from collections import deque
+def main():
 
-q=deque(['a','3','3'])
-clients = []
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    my_socket.bind((MASTER_HOST, MASTER_PORT))
+    my_socket.listen(NUM_SLAVES)
 
-def clientHandler(c, addr):
-    global clients
-    print(addr, "is Connected")
-    
-    while True:
-        data = c.recv(1024) # ready
-        print(repr(data))
-        if not data: 
-            break 
-        if data.decode() =="ready":
-            nd=q.popleft()
-            c.sendall(nd.encode())
-        elif data.decode() =="finished":
-            break
+    print("Server is running on " + str(MASTER_PORT))
 
-   
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.bind((MASTER_HOST, MASTER_PORT))
-s.listen(NUM_SLAVES)
+    list_connections = []
 
-print("Server is running on "+ str(MASTER_PORT))
+    for i in range(NUM_SLAVES):
+        c, addr = my_socket.accept()
+        list_connections.append((c, addr))
 
-#Thread(target=clientHandler).start()
-#Thread(target=clientHandler).start()
-#Thread(target=clientHandler).start()
-trds = []
+    retrived_list = ['a'*x for x in range(1, 10)]
+    send_list = [[] for x in range(NUM_SLAVES)]
 
-ox=[]
+    for i, v in enumerate(retrived_list):
+        send_list[i % NUM_SLAVES].append(v)
 
-for i in range(NUM_SLAVES): 
-    c, addr = s.accept() 
-    clients.append(addr)
-    ox.append((c,addr))
+    for j, pair in enumerate(list_connections):
+        packet = pickle.dumps(send_list[j])
+        length = struct.pack('!I', len(packet))
+        packet = length + packet
+        pair[0].sendall(packet)
 
-for i in ox:
-    t = Process(target=clientHandler, args = (i[0], i[1]))
-    trds.append(t)
-    t.start()
+    my_socket.close()
+    return 0
 
-for t in trds:
-    t.join()
-
-s.close()
+if __name__ == "__main__":
+    main()
--- src/slave.py
+++ src/slave.py
@@ -11,33 +11,25 @@ MASTER_HOST = settings['master']['ip']  # The server's hostname or IP address
 MASTER_PORT = settings['master']['port']        # The port used by the server
 
 
-def aux():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        #s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                buf = b''
-                while len(buf) < 4:
-                    buf += conn.recv(4 - len(buf))
-                length = struct.unpack('!I', buf)[0]
-                data = conn.recv(length)
-                data_arr = pickle.loads(data)
-                print('Received', repr(data_arr))
-                break
-
 def connect():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((MASTER_HOST, MASTER_PORT))
-        s.sendall(b'ready')
-        sn = s.recv(1024)
-        print('crawling',repr(sn))
-        s.sendall(b'finished')
+        while True:
+            buf = b''
+            while len(buf) < 4:
+                buf += s.recv(4 - len(buf))
+            length = struct.unpack('!I', buf)[0]
+            data = s.recv(length)
+            data_arr = pickle.loads(data)
+            break
+
+        for i in data_arr:
+            print('crawling', i)
 
 
 def main():
     connect()
+
+
 if __name__ == "__main__":
     main()
