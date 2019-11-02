912ed2daeb10358bfb6bd212bd0167410479ce59
RamonRomeroTec <rr100@live.com.mx>
2019-11-02 00:18:32 -0600

    queue working

--- src/master.py
+++ src/master.py
@@ -10,17 +10,52 @@ SLAVE_SOCKETS = [(s['ip'], s['port']) for s in settings['slaves']]
 MASTER_HOST = settings['master']['ip']  # The server's hostname or IP address
 MASTER_PORT = settings['master']['port']        # The port used by the server
 
-retrived_list = ['a'*x for x in range(1, 10)]
-send_list = [[] for x in range(NUM_SLAVES)]
+from multiprocessing import Process
+from collections import deque
 
-for i, v in enumerate(retrived_list):
-    send_list[i % NUM_SLAVES].append(v)
+q=deque(['a','3','3'])
+clients = []
 
+def clientHandler(c, addr):
+    global clients
+    print(addr, "is Connected")
+    
+    while True:
+        data = c.recv(1024) # ready
+        print(repr(data))
+        if not data: 
+            break 
+        if data.decode() =="ready":
+            nd=q.popleft()
+            c.sendall(nd.encode())
+        elif data.decode() =="finished":
+            break
 
-for j, pair in enumerate(SLAVE_SOCKETS):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(pair)
-        packet = pickle.dumps(send_list[j])
-        length = struct.pack('!I', len(packet))
-        packet = length + packet
-        s.sendall(packet)
+   
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.bind((MASTER_HOST, MASTER_PORT))
+s.listen(NUM_SLAVES)
+
+print("Server is running on "+ str(MASTER_PORT))
+
+#Thread(target=clientHandler).start()
+#Thread(target=clientHandler).start()
+#Thread(target=clientHandler).start()
+trds = []
+
+ox=[]
+
+for i in range(NUM_SLAVES): 
+    c, addr = s.accept() 
+    clients.append(addr)
+    ox.append((c,addr))
+
+for i in ox:
+    t = Process(target=clientHandler, args = (i[0], i[1]))
+    trds.append(t)
+    t.start()
+
+for t in trds:
+    t.join()
+
+s.close()
--- src/slave.py
+++ src/slave.py
@@ -1,24 +1,43 @@
-import socket
-import sys
-import pickle
 import struct
+import json
+import socket
+import pickle
+with open('./settings.json') as f:
+    settings = json.load(f)
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-# Port to listen on (non-privileged ports are > 1023)
-PORT = int(sys.argv[1])
+NUM_SLAVES = len(settings['slaves'])
+SLAVE_SOCKETS = [(s['ip'], s['port']) for s in settings['slaves']]
+MASTER_HOST = settings['master']['ip']  # The server's hostname or IP address
+MASTER_PORT = settings['master']['port']        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            buf = b''
-            while len(buf) < 4:
-                buf += conn.recv(4 - len(buf))
-            length = struct.unpack('!I', buf)[0]
-            data = conn.recv(length)
-            data_arr = pickle.loads(data)
-            print('Received', repr(data_arr))
-            break
+
+def aux():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                buf = b''
+                while len(buf) < 4:
+                    buf += conn.recv(4 - len(buf))
+                length = struct.unpack('!I', buf)[0]
+                data = conn.recv(length)
+                data_arr = pickle.loads(data)
+                print('Received', repr(data_arr))
+                break
+
+def connect():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((MASTER_HOST, MASTER_PORT))
+        s.sendall(b'ready')
+        sn = s.recv(1024)
+        print('crawling',repr(sn))
+        s.sendall(b'finished')
+
+
+def main():
+    connect()
+if __name__ == "__main__":
+    main()
