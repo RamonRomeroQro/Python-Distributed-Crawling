9bd0051e35be2513cb4616d3ace1937ace4d3011
RamonRomeroTec <rr100@live.com.mx>
2019-11-20 01:53:48 -0600

    set to test

--- test/src/generate.py
+++ test/src/generate.py
@@ -1,3 +1,8 @@
 import os
-a=os.listdir("./resized")
+images=os.listdir("./resized")
+for name in images:
+    i=len(name)-1
+    while(i>=0):
+        i-=1
+        print(name) 
 print("\n".join(a))
